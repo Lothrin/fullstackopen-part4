0c1947074b6bf5d124f42c047c3c837e257f0db2
Lothrin <m.erkam.arafat@gmail.com>
2024-10-26 18:57:49 +0200

    4.5*: Helper Functions and Unit Tests, step 3

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -5,7 +5,24 @@ const dummy = (blogs) => {
 const totalLikes = (blogs) => {
     return blogs.reduce((sum, blog) => sum + blog.likes, 0);
 }
+
+const favoriteBlog = (blogs) => {
+    if (blogs.length === 0) {
+        return null;
+    }
+  
+    const favorite = blogs
+      .sort((a, b) => b.likes - a.likes)[0];
+  
+    return {
+      title: favorite.title,
+      author: favorite.author,
+      likes: favorite.likes,
+    };
+  }
+  
   module.exports = {
     dummy,
     totalLikes,
+    favoriteBlog,
   }
